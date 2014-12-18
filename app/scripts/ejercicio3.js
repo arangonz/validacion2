169ec37a7cba71154e76734d5520b9b3fb1e1a5a
francisco <francisco.arangonz@gmail.com>
2014-12-18 13:01:45 +0100

    ejercicio3 y ejercicio4 resueltos

---
+++ app/scripts/ejercicio3.js
@@ -0,0 +1,21 @@
+ $('#formulario').validate({
+     onkeyup: false,
+     onfocusout: false,
+     onclick: false,
+     rules: {
+         nombre: {
+             required: true,
+             minlength: 2,
+             remote: 'php/users.php'
+         },
+         email: {
+             required: true,
+             remote: 'php/emails.php'
+         },
+         comentarios: 'required',
+         cuenta:{
+            iban:true,
+            required:true
+         }
+     }
+ });
